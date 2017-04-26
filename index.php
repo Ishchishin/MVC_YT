5c31cb17b8649f35d2b2ac0fccbbcb00f050e44b
Sergey Ishchishin <dolphin_i@ukr.net>
2017-04-26 23:23:36 +0300

    comit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// front Controller
+
+
+// 1. общие настройки
+    ini_set('display_errors',1);
+    error_reporting(E_ALL);
+
+
+// 2. Подключение файлов системы
+    define('ROOT',dirname(__FILE__));
+    require_once (ROOT.'/components/Router.php');
+
+// 3. Установка соединения с БД
+
+
+
+// 4. Вызов router
+$router = new Router();
+$router->run();
